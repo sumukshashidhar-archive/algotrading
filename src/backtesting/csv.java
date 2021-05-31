d130202e5d478d57d62fd07eb5d624a8509cb9ea
SGZ-7 <85120275+SGZ-7@users.noreply.github.com>
2021-06-01 01:46:28 +0530

    Update csv.java

--- src/backtesting/csv.java
+++ src/backtesting/csv.java
@@ -5,7 +5,7 @@ class csv
 public static void main(String args[]) throws Exception
 {  
 Scanner sc = new Scanner(new File("file location"));  
-sc.useDelimiter(";");   
+sc.useDelimiter(",");   
 while (sc.hasNext()) 
 {  
 System.out.print(sc.next());  
