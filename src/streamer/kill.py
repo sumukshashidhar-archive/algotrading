a7e86b07fa21555de9727141c1616fe823d46f87
sumukshashidhar <sumukuuu@gmail.com>
2021-06-16 22:52:22 +0530

    feat: update kill script

--- src/streamer/kill.py
+++ src/streamer/kill.py
@@ -1,2 +1,8 @@
 import os
+import datetime
+import time
+from pytz import timezone
+time.sleep(50)
+strtime = datetime.now(timezone("UTC")).astimezone(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+os.system(f'cd realtime && git add . && git commit -m "{strtime}" && git push')
 os.system('pkill -9 python3')
